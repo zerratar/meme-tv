0d2924273d8301de35d506a81561ab1674622fb1
zerratar <zerratar@gmail.com>
2019-09-02 19:13:05 +0200

    Fix broken templates

---
+++ src/MemeTV.BusinessLogic/ClipProvider.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using MemeTV.Models;
+using Microsoft.AspNetCore.Hosting;
+using Newtonsoft.Json;
+
+namespace MemeTV.BusinessLogic
+{
+    public class ClipProvider : IClipProvider
+    {
+        private readonly IHostingEnvironment env;
+        private readonly IReadOnlyList<Clip> clips;
+        public ClipProvider(IHostingEnvironment env)
+        {
+            this.env = env;
+            var file = System.IO.Path.Combine(this.env.WebRootPath, "data", "subtitle-timings.json");
+            clips = JsonConvert.DeserializeObject<List<Clip>>(System.IO.File.ReadAllText(file));
+        }
+
+        public Clip Get(string name)
+        {
+            return this.clips.FirstOrDefault(x => x.Name.Equals(name, StringComparison.OrdinalIgnoreCase));
+        }
+    }
+}
---
+++ src/MemeTV.Models/Clip.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MemeTV.Models
+{
+    public class Clip
+    {
+        public string Name { get; set; }        
+        public string Webm => "/data/clips/webm/" + Name + ".webm";
+        public string Mp4 => "/data/clips/mp4/" + Name + ".mp4";
+        public string Image => "/data/clips/images/" + Name + ".jpg";
+        public string ImageBig => "/data/clips/images/grande" + Name + ".jpg";
+        public float Duration { get; set; }
+        public float Audiodelay { get; set; }
+        public IReadOnlyList<CaptionCue> CaptionCues { get; set; }
+    }
+}
